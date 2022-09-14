146e1b264a5e468979d12d25e267d00d43dc8efc
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-09-14 11:25:11 -0300

    Primeira Aula concluida

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+//ecmascript MODULES altera a extensão do arquivo e o packge .json
+import express from 'express';
+
+
+const app = express();
+
+app.get('/ads', (request, response) => {
+
+    return response.json([
+        { id: 1, name: 'Anuncio 1' },
+        { id: 2, name: 'Anuncio 2' },
+        { id: 3, name: 'Anuncio 3' },
+    ]);
+});
+
+app.listen(3000);
